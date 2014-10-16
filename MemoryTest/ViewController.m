1f4c0b6b80faf3fb06784d1c88a179b98910e91b
Frank Schlegel <frank.schlegel@me.com>
2014-10-16 11:31:37 +0200

    Added a Photo Extension that uses the same interface.

--- MemoryTest/ViewController.m
+++ MemoryTest/ViewController.m
@@ -10,9 +10,20 @@
 #import <sys/types.h>
 #import <sys/sysctl.h>
 
+#ifdef PHOTO_EXTENSION_BUILD
+#import <Photos/Photos.h>
+#import <PhotosUI/PhotosUI.h>
+#endif
+
 #define CRASH_MEMORY_FILE_NAME @"CrashMemory.dat"
 #define MEMORY_WARNINGS_FILE_NAME @"MemoryWarnings.dat"
 
+
+#ifdef PHOTO_EXTENSION_BUILD
+@interface ViewController () <PHContentEditingController>
+@property (strong) PHContentEditingInput *input;
+@end
+#endif
 
 @interface ViewController () {
     
@@ -211,6 +222,57 @@
     [timer invalidate];
     timer = [NSTimer scheduledTimerWithTimeInterval:0.02 target:self selector:@selector(allocateMemory) userInfo:nil repeats:YES];
 }
+
+
+#ifdef PHOTO_EXTENSION_BUILD
+
+#pragma mark - PHContentEditingController
+
+- (BOOL)canHandleAdjustmentData:(PHAdjustmentData *)adjustmentData {
+    // Inspect the adjustmentData to determine whether your extension can work with past edits.
+    // (Typically, you use its formatIdentifier and formatVersion properties to do this.)
+    return NO;
+}
+
+- (void)startContentEditingWithInput:(PHContentEditingInput *)contentEditingInput placeholderImage:(UIImage *)placeholderImage {
+    // Present content for editing, and keep the contentEditingInput for use when closing the edit session.
+    // If you returned YES from canHandleAdjustmentData:, contentEditingInput has the original image and adjustment data.
+    // If you returned NO, the contentEditingInput has past edits "baked in".
+    self.input = contentEditingInput;
+}
+
+- (void)finishContentEditingWithCompletionHandler:(void (^)(PHContentEditingOutput *))completionHandler {
+    // Update UI to reflect that editing has finished and output is being rendered.
+
+    // Render and provide output on a background queue.
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // Create editing output from the editing input.
+        PHContentEditingOutput *output = [[PHContentEditingOutput alloc] initWithContentEditingInput:self.input];
+
+        // Provide new adjustments and render output to given location.
+        // output.adjustmentData = <#new adjustment data#>;
+        // NSData *renderedJPEGData = <#output JPEG#>;
+        // [renderedJPEGData writeToURL:output.renderedContentURL atomically:YES];
+
+        // Call completion handler to commit edit to Photos.
+        completionHandler(output);
+
+        // Clean up temporary files, etc.
+    });
+}
+
+- (BOOL)shouldShowCancelConfirmation {
+    // Returns whether a confirmation to discard changes should be shown to the user on cancel.
+    // (Typically, you should return YES if there are any unsaved changes.)
+    return NO;
+}
+
+- (void)cancelContentEditing {
+    // Clean up temporary files, etc.
+    // May be called after finishContentEditingWithCompletionHandler: while you prepare output.
+}
+
+#endif
 
 @end
 
